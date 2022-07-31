4adf96f3f08c875907116c73cf8bf7449a858fc8
yew <winstonye@hotmail.ca>
2022-07-31 15:25:00 -0700

    Added Firebase auth dependencies

--- app/src/main/java/com/ancientones/squadup/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/ancientones/squadup/ui/profile/ProfileFragment.kt
@@ -8,10 +8,13 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.ancientones.squadup.databinding.FragmentNotificationsBinding
+import com.google.firebase.analytics.FirebaseAnalytics
+
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -32,6 +35,14 @@ class ProfileFragment : Fragment() {
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(requireActivity())
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "12345")
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "hello_world_name")
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "test_string")
+        mFirebaseAnalytics!!.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+
         return root
     }
 
