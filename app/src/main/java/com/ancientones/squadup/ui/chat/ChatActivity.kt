bac69a4eec2c43b1b07650919b35eb5c7038eb8b
VJ <vbakhshi@sfu.ca>
2022-08-01 21:06:37 -0700

    chat activity changed code to reflect last commit(idk something messed up. also added tab title for chat to string.xml

--- app/src/main/java/com/ancientones/squadup/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/ancientones/squadup/ui/chat/ChatActivity.kt
@@ -1,35 +1,23 @@
 package com.ancientones.squadup.ui.chat
 
-import androidx.lifecycle.ViewModelProvider
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.ancientones.squadup.R
-import com.ancientones.squadup.databinding.FragmentChatBinding
-import com.ancientones.squadup.databinding.FragmentDashboardBinding
+import androidx.lifecycle.ViewModelProvider
+import com.ancientones.squadup.databinding.ActivityChatBinding
 
-class ChatFragment : Fragment() {
-
-    private var _binding: FragmentChatBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    private lateinit var viewModel: ChatViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        viewModel = ViewModelProvider(this).get(ChatViewModel::class.java);
-
-        _binding = FragmentChatBinding.inflate(inflater, container, false);
-        val root: View = binding.root;
-        return root;
+class ChatActivity : AppCompatActivity() {
+    companion object {
+        val CHAT_ID_KEY = "chat_id_key";
     }
 
+    private lateinit var binding: ActivityChatBinding;
+    private lateinit var viewModel: ChatViewModel;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ChatViewModel::class.java);
+
+        binding = ActivityChatBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+    }
 }
