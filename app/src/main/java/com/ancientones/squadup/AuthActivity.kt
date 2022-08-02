7532fd335e039e56fd467aa0f2778378bc9bf16a
jdauz <jdauz@sfu.ca>
2022-08-01 22:59:53 -0700

    Functional sign up

--- app/src/main/java/com/ancientones/squadup/AuthActivity.kt
+++ app/src/main/java/com/ancientones/squadup/AuthActivity.kt
@@ -1,7 +1,11 @@
 package com.ancientones.squadup
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.ancientones.squadup.auth.AuthSignInActivity
+import com.ancientones.squadup.auth.AuthSignUpActivity
 import com.ancientones.squadup.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
@@ -13,5 +17,10 @@ class AuthActivity : AppCompatActivity() {
 
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun onClick(view: View) {
+        val intent = Intent(this, AuthSignUpActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/ancientones/squadup/User.kt
@@ -0,0 +1,11 @@
+package com.ancientones.squadup
+
+class User (email: String, fName: String, lName: String) {
+    public var firstName: String = fName
+    public var lastName: String = lName
+    public var email: String = email
+
+    class User() {
+
+    }
+}
