39846f0fb36b34183fec0882e168f9040922b2cb
yew <winstonye@hotmail.ca>
2022-08-02 17:57:54 -0700

    Fix viewmodel crash bug

--- app/src/main/java/com/ancientones/squadup/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/ancientones/squadup/ui/profile/ProfileFragment.kt
@@ -3,6 +3,7 @@ package com.ancientones.squadup.ui.profile
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.TextView
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
@@ -18,41 +19,24 @@ class ProfileFragment : Fragment() {
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
-    // TODO: add rest of ProfileViewModel user details
-    // User Profile details
     private lateinit var profileViewModel: ProfileViewModel
-    private var firstName: String = ""
-    private var lastName: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        profileViewModel.fetchUser()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        profileViewModel.firstName.observe(requireActivity()) {
-            firstName = it
-            if(firstName.isNotEmpty() && lastName.isNotEmpty()) {
-                binding.username.text = "$firstName $lastName"
-            }
-        }
-
-        profileViewModel.lastName.observe(requireActivity()) {
-            lastName = it
-            if(firstName.isNotEmpty() && lastName.isNotEmpty()) {
-                binding.username.text = "$firstName $lastName"
-            }
-        }
+        profileViewModel.fetchUser()
 
         return root
     }
 
-    // Adds an edit button in toolbar
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // Adds an edit button in toolbar
         val menuHost: MenuHost = requireActivity()
         menuHost.addMenuProvider(object : MenuProvider {
             override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
@@ -62,14 +46,23 @@ class ProfileFragment : Fragment() {
             // Handle the menu selection
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
                 if (menuItem.itemId == R.id.editMenuBtn) {
-                    // TODO: uncomment when EditProfileActivity is ready
                     val intent = Intent(requireActivity(), EditProfileActivity::class.java)
-                    intent.putExtra("full_name", "${firstName} ${lastName}")
+                    intent.putExtra("full_name", "${profileViewModel.firstName.value} ${profileViewModel.lastName.value}")
                     startActivity(intent)
                 }
                 return true
             }
         }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        val usernameView = view.findViewById<TextView>(R.id.username)
+
+        profileViewModel.firstName.observe(requireActivity()) {
+            usernameView.text = "${profileViewModel.firstName.value} ${profileViewModel.lastName.value}"
+        }
+
+        profileViewModel.lastName.observe(requireActivity()) {
+            usernameView.text = "${profileViewModel.firstName.value} ${profileViewModel.lastName.value}"
+        }
     }
 
     override fun onDestroyView() {
