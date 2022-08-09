17d275a7520406ae7d27fc9af78907f0e34194db
VJ <vbakhshi@sfu.ca>
2022-08-08 18:19:41 -0700

    updated chat model to include drop in id

--- app/src/main/java/com/ancientones/squadup/database/models/Chat.kt
+++ app/src/main/java/com/ancientones/squadup/database/models/Chat.kt
@@ -5,7 +5,7 @@ import com.google.firebase.firestore.DocumentId
 data class Chat (
     @DocumentId
     val id: String = "",
+    val dropIn_id: String = "",
     val title: String = ""
-    ){}
-
-
+    ){
+}
